311ec457a33c4772e53845acbea8c8470a6c1f7a
thiagocostta <108770314+thiagocostta@users.noreply.github.com>
2024-03-07 17:27:03 +0000

    resolucao exercico

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,14 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'Thiago'
+profissão = 'Atendente'
 
 # Declare variáveis de cada tipo numérico
-
+idade = 27
+salario = 1.600
+complexo = 2+2j
 
 # Declare uma variável do tipo booleano
+verdadeiro = True
+falso = False
