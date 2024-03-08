700d2afd768596ae373d3b68f01f3ba2be6b0f0a
thiagocostta <108770314+thiagocostta@users.noreply.github.com>
2024-03-08 12:13:54 +0000

    resolucao exerciocios

--- exercicios/1-primeiros-passos/4-operadores.py
+++ exercicios/1-primeiros-passos/4-operadores.py
@@ -2,9 +2,35 @@ ano_nascimento = 1989
 ano_formatura = 2010
 
 # Considerando que as variáveis acima correspondem a 'Gerlaine', descubra a idade dela no ano da sua formatura
-
+dataN = int(1989)
+dataF = int(2010)
+idade = dataF - dataN
+print ("Sua idade é {}".format(idade))
 
 # Escreva expressões comparativas usando os operadores relacionais >, <= e ==. Imprima na tela as respostas
+a = 5
+b = 9
+resultado = a > b
+resultado2 = a <= b
+resultado3 = a == b
 
+print (resultado3)
+print (resultado2)
+print (resultado)
 
 # Crie expressões comparativas mais complexas utilizando operadores lógicos and, or e not. Imprima na tela as respostas
+resultado = 3 == 3 and 2== 2
+resultado1 = 3 == 3 and 2 == 1
+resultado2 = 3 == 3 or 2 == 2 
+resultado3 = 3 == 2 or 2 == 3
+resultado4 = not 3 == 3
+resultado5 = not 3 == 1 
+
+print (resultado)
+print (resultado1)
+print (resultado2)
+print (resultado3)
+print (resultado4)
+print (resultado5)
+
+#Obs: sempre realizo as operações e testo os códigos no vscode, depois apenas colo os resultados aqui. 
