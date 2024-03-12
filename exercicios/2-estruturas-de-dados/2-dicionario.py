e419aa359a92a0ea423824c632fa00fa2f1db395
thiagocostta <108770314+thiagocostta@users.noreply.github.com>
2024-03-12 10:51:27 +0000

    resolucao dicionario

--- exercicios/2-estruturas-de-dados/2-dicionario.py
+++ exercicios/2-estruturas-de-dados/2-dicionario.py
@@ -1,18 +1,20 @@
-pessoa = {'nome':'Crislaine', 
-          'idade':46, 
-          'ano_formatura':2010, 
-          'linguagens_programacao':['python', 'r', 'javascript', 'ruby'], 
-          'brasileira':True, 
-          'hobby':['nadar', 'ler', 'pedalar'], 
-          'animal_estimacao':False}
+pessoa = {"nome" : "Thiago", 
+          "idade" : 27, 
+          "ano_formatura" : 2017, 
+          "linguagens_de_program": "python",
+          "brasileiro": True,
+          "hobby": ["surfar","correr","xadres","muscia","pintar"],
+          "animal_estimacao": True}
 
 # Imprima na tela o valor equivalente a chave "hobby"
-
+print (pessoa["hobby"])
 
 # Imprima na tela uma lista apenas com os valores do dicionário
-
+print (pessoa.values())
 
 # Imprima na tela uma lista apenas com as chaves do dicionário
-
+print (pessoa.keys())
 
 # Insira um novo par chave-valor no dicionário
+pessoa["ano_nasc"] = 1996
+print (pessoa)
